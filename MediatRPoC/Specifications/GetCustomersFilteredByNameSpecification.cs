e7320bb8248817e150c743cb9dde6057f9ba7dc1
Kevin Gates <6576422+kevingates@users.noreply.github.com>
2024-05-17 15:46:02 -0400

    Improved Name of Specification

---
+++ MediatRPoC/Specifications/GetCustomersFilteredByNameSpecification.cs
@@ -0,0 +1,20 @@
+﻿using MediatR;
+using MediatRPoC.DTOs;
+using MediatRPoC.Entities;
+
+namespace MediatRPoC.Specifications
+{
+	internal class GetCustomersFilteredByNameSpecification : Specification<Customer, CustomerDto>
+	{
+		public GetCustomersFilteredByNameSpecification(string name)
+		{
+			Where(c => c.Name.Contains(name))
+				.OrderByExpression(q => q.OrderBy(x => x.Name))
+				.SelectExpression(c => new CustomerDto
+				{
+					Id = c.Id,
+					Name = c.Name
+				});
+		}
+	}
+}
